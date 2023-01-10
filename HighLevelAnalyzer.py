668ea451c904317ff57dc8617a4968b1a4277fb4
Drihan du Preez <drihan@btinternet.com>
2023-01-10 17:37:58 +0000

    now displays 302 & 303 CAN values

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -86,36 +86,102 @@ class Hla(HighLevelAnalyzer):
                         'input_type': lat,
                     })
 
-        # elif frame.type == "data_field" and self.id == 770:
-        #     data_byte = hex(frame.data["data"][0])
-        #     data_byte = data_byte[2:]
-        #     if len(data_byte) == 1:
-        #         new_data_byte = "0" + data_byte
-        #         data_list.append(new_data_byte)
-        #     else:
-        #         data_list.append(data_byte)
-        #     frame_start_time.append(frame.start_time)
-        #     frame_end_time.append(frame.end_time)
-        #     if len(data_list) == 4:
-        #         hex_join = "".join(data_list[:4])
-        #         converted_hex = int(hex_join, 16)
-        #         long = converted_hex / 100000
-        #         return AnalyzerFrame('message_information', frame_start_time[0], frame_end_time[3], {
-        #                 'info': 'Longitude',
-        #                 'input_type': long,
-        #             })
-        #     if len(data_list) == 6:
-        #         hex_join = "".join(data_list[4:6])
-        #         converted_hex = int(hex_join, 16)
-        #         speed = converted_hex * 0.01 * 1.852 # convert knots to km/h
-        #         return AnalyzerFrame('message_information', frame_start_time[0], frame_end_time[3], {
-        #                 'info': 'Speed',
-        #                 'input_type': str(speed) + "km/h",
-        #             })
-                
-        # end of CAN message
-        # if frame.type == "crc_field":
-        #     self.temp_frame.end_time = frame.end_time
-        #     new_frame = self.temp_frame
-        #     self.temp_frame = None
-        #     return new_frame
+        elif frame.type == "data_field" and self.id == 770:
+            data_byte = hex(frame.data["data"][0])
+            data_byte = data_byte[2:]
+            if len(data_byte) == 1:
+                new_data_byte = "0" + data_byte
+                data_list.append(new_data_byte)
+            else:
+                data_list.append(data_byte)
+            frame_start_time.append(frame.start_time)
+            frame_end_time.append(frame.end_time)
+            if len(data_list) == 4:
+                hex_join = "".join(data_list[:4])
+                converted_hex = int(hex_join, 16)
+                long = converted_hex / 100000
+                return AnalyzerFrame('message_information', frame_start_time[0], frame_end_time[3], {
+                        'info': 'Longitude',
+                        'input_type': long,
+                    })
+            elif len(data_list) == 6:
+                hex_join = "".join(data_list[4:6])
+                converted_hex = int(hex_join, 16)
+                speed = converted_hex * 0.01 * 1.852 # convert knots to km/h
+                return AnalyzerFrame('message_information', frame_start_time[4], frame_end_time[5], {
+                        'info': 'Speed',
+                        'input_type': str(round(speed, 3)) + "km/h",
+                    })
+            elif len(data_list) == 8:
+                hex_join = "".join(data_list[6:])
+                converted_hex = int(hex_join, 16)
+                heading = converted_hex * 0.01
+                last_start_time = frame_start_time[6]
+                last_end_time = frame_end_time[7]
+                data_list = []
+                frame_start_time = []
+                frame_end_time = []
+                return AnalyzerFrame('message_information', last_start_time, last_end_time, {
+                        'info': 'Heading',
+                        'input_type': str(round(heading, 3)) + "°"
+                    })
+
+        elif frame.type == "data_field" and self.id == 771:
+            data_byte = hex(frame.data["data"][0])
+            data_byte = data_byte[2:]
+            if len(data_byte) == 1:
+                new_data_byte = "0" + data_byte
+                data_list.append(new_data_byte)
+            else:
+                data_list.append(data_byte)
+            frame_start_time.append(frame.start_time)
+            frame_end_time.append(frame.end_time)
+            if len(data_list) == 3:
+                hex_join = "".join(data_list[:3])
+                converted_hex = int(hex_join, 16)
+                altitude = converted_hex * 0.01
+                return AnalyzerFrame('message_information', frame_start_time[0], frame_end_time[2], {
+                        'info': 'Altidude',
+                        'input_type': str(round(altitude, 3)) + "m",
+                    })
+            if len(data_list) == 5:
+                hex_join = "".join(data_list[3:5])
+                converted_hex = int(hex_join, 16)
+                vertical_velocity = converted_hex * 0.01
+                return AnalyzerFrame('message_information', frame_start_time[3], frame_end_time[4], {
+                        'info': 'Vertical Velocity',
+                        'input_type': str(round(vertical_velocity, 3)) + "m/s",
+                    })
+            if len(data_list) == 7:
+                if data_list[6] == "04":
+                    bit_info = "VBOX3"
+                elif data_list[6] == "08":
+                    bit_info = "logging"
+                elif data_list[6] == "0c":
+                    bit_info = "VBOX3 & Logging"
+                else:
+                    bit_info = "Not specified"
+                return AnalyzerFrame('message_information', frame.start_time, frame.end_time, {
+                        'info': 'VBOX information',
+                        'input_type': bit_info,
+                    })
+            if len(data_list) == 8:
+                if data_list[7] == "01":
+                    status_info = "No additional information"
+                elif data_list[7] == "05":
+                    status_info = "Brake test started"
+                elif data_list[7] == "09":
+                    status_info = "Brake Trigger active"
+                elif data_list[7] == "11":
+                    status_info = "DGPS active"
+                elif data_list[7] == "21":
+                    status_info = "Dual lock"
+                else:
+                    status_info = "Not stated"
+                data_list = []
+                frame_start_time = []
+                frame_end_time = []
+                return AnalyzerFrame('message_information', frame.start_time, frame.end_time, {
+                        'info': 'Status information',
+                        'input_type': status_info,
+                    })
